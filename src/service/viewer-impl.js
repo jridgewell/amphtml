863eff2a9ee56432c8291a40d08ee2b1e84cdc1f
Justin Ridgewell <jridgewell@google.com>
2016-09-19 15:56:49 -0400

    Fix DeMorgan's law
    This would have changed any `HIDDEN`s to `VISIBLE`. The intention is to
    change anything "unknown" to `VISIBLE`.

--- src/service/viewer-impl.js
+++ src/service/viewer-impl.js
@@ -552,7 +552,7 @@ export class Viewer {
       }
     } else if (!getMode().localDev) {
       // When not embedded, only VISIBLE and HIDDEN states are allowed.
-      if (state !== VisibilityState.VISIBLE ||
+      if (state !== VisibilityState.VISIBLE &&
           state !== VisibilityState.HIDDEN) {
         state = VisibilityState.VISIBLE;
       }
